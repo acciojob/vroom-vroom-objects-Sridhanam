dcca9111d017d03fd1a1c6b52f6285e9b89c2ec8
Sridhanam <dharsri255@gmail.com>
2025-04-15 23:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function Car(make, model) {
-    this.make = make;
-    this.model = model;
+  this.make = make;
+  this.model = model;
 }
 
 // Adding method to Car prototype
-Car.prototype.getMakeModel = function () {
-    return ${this.make} ${this.model};
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
 };
 
 // SportsCar constructor function inheriting from Car
 function SportsCar(make, model, topSpeed) {
-    // Call the Car constructor to inherit properties
-    Car.call(this, make, model);
-    this.topSpeed = topSpeed;
+  // Call the Car constructor to inherit properties
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
 }
 
 // Inherit Car prototype methods
@@ -22,10 +22,10 @@ SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
 // Adding method to SportsCar prototype
-SportsCar.prototype.getTopSpeed = function () {
-    return this.topSpeed;
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
 };
 
-// Do not change the code below
+// Expose the constructors
 window.Car = Car;
 window.SportsCar = SportsCar;
